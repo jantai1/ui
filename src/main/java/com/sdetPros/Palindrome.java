3b52e4d41272e13d6fd8e2d223a0d0ce03648177
jantai1 <jantaimonol@gmail.com>
2023-01-26 22:17:22 -0500

    add Palindrome.class

---
+++ src/main/java/com/sdetPros/Palindrome.java
@@ -0,0 +1,15 @@
+package com.sdetPros;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPalindrome(final int number){
+
+        int
+
+        return false;
+    }
+}
